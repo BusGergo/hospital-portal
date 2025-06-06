1fb7b29ddf3bcac7c01265ed65c879de16709843
Gergő-Vince Bús <bus.gergo@gmail.com>
2025-06-07 01:59:48 +0300

    Stylized the sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,57 +1,67 @@
-import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
-import "./globals.css";
-import Link from "next/link";
-import { Input } from "@/components/ui/input";
+import type { Metadata } from 'next';
+import { Nunito } from 'next/font/google';
+import './globals.css';
+import Link from 'next/link';
+import { Input } from '@/components/ui/input';
+import {
+	BriefcaseMedical,
+	CalendarFold,
+	LayoutDashboard,
+	UserRoundPen,
+} from 'lucide-react';
 
 const nunito = Nunito({
-  variable: "--font-nunito",
-  subsets: ["latin"],
+	variable: '--font-nunito',
+	subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Lifeline",
+	title: 'Lifeline',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${nunito.variable} antialiased`}>
-        <div className="flex h-screen font-[family-name:var(--font-nunito)] bg-[#1c4f46]">
-          <aside className="w-50 bg-[#1c4f46] text-white p-4 space-y-4">
-            <h2 className="text-xl font-bold">Lifeline</h2>
-            <Input type="email" placeholder="Search" />
-            <nav className="space-y-2">
-              <Link href="/dashboard" className="block hover:underline">
-                Dashboard
-              </Link>
-              <Link href="/appointments" className="block hover:underline">
-                Appointments
-              </Link>
-              <Link href="/doctors" className="block hover:underline">
-                Doctors
-              </Link>
-              <Link href="/profile" className="block hover:underline">
-                Profile
-              </Link>
-              <Link
-                href="/logout"
-                className="block hover:underline text-red-300"
-              >
-                Logout
-              </Link>
-            </nav>
-          </aside>
+	return (
+		<html lang="en">
+			<body className={`${nunito.variable} antialiased`}>
+				<div className="flex h-screen font-[family-name:var(--font-nunito)] bg-[var(--color-background)]">
+					<aside className="w-55 bg-[var(--color-background)] text-white p-4 space-y-4">
+						<h2 className="text-xl font-bold text-black">Lifeline</h2>
+						<Input type="email" placeholder="Search" />
+						<nav className="flex flex-col gap-5">
+							<Link
+								href="/dashboard"
+								className="text-black flex gap-3 font-bold"
+							>
+								<LayoutDashboard />
+								Dashboard
+							</Link>
+							<Link
+								href="/appointments"
+								className="text-black flex gap-3 font-bold"
+							>
+								<CalendarFold />
+								Appointments
+							</Link>
+							<Link href="/doctors" className="text-black flex gap-3 font-bold">
+								<BriefcaseMedical />
+								Doctors
+							</Link>
+							<Link href="/profile" className="text-black flex gap-3 font-bold">
+								<UserRoundPen />
+								Profile
+							</Link>
+						</nav>
+					</aside>
 
-          <main className="flex-1 bg-gray-50 p-6 overflow-y-auto rounded-xl m-2">
-            {children}
-          </main>
-        </div>
-      </body>
-    </html>
-  );
+					<main className="flex-1 bg-gray-50 p-6 overflow-y-auto">
+						{children}
+					</main>
+				</div>
+			</body>
+		</html>
+	);
 }
